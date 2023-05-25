2f640a1a11f5b6cee7eb1171b3adaba4143d4448
llay <ilay2018rassi@gmail.com>
2023-05-25 19:56:28 +0300

    header

---
+++ blanks/js/blanks_header.js
@@ -0,0 +1,20 @@
+const phoneNumberElements = document.querySelectorAll('.your-city.city-header');
+
+phoneNumberElements.forEach(phoneNumberElement => {
+    phoneNumberElement.addEventListener('click', () => {
+        const phoneNumber = phoneNumberElement.textContent.trim();
+
+        const dummy = document.createElement('input');
+        dummy.style.display = 'none';
+        document.body.appendChild(dummy);
+
+        dummy.value = phoneNumber;
+        dummy.select();
+
+        document.execCommand('copy');
+
+        document.body.removeChild(dummy);
+
+        alert(`Номер телефона ${phoneNumber} скопирован в буфер обмена`);
+    });
+});
